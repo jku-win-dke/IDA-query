8b2b1fcd00e17d9ddfdcd42cf51bcd58457214a7
schuetz <schuetz@dke.uni-linz.ac.at>
2022-11-02 12:57:08 +0100

    Add info

--- src/main/java/at/jku/dke/ida/idaquery/IdaQueryApplication.java
+++ src/main/java/at/jku/dke/ida/idaquery/IdaQueryApplication.java
@@ -1,9 +1,14 @@
 package at.jku.dke.ida.idaquery;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(title = "REST Endpoint for IDA Query Engine", version = "1.0", description = "The REST endpoint for executing queries from IDA.")
+)
 public class IdaQueryApplication {
 
     public static void main(String[] args) {
