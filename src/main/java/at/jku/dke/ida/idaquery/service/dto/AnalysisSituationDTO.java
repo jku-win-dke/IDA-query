9f010ab1e2838b341831a0ef3c22f13860074be4
schuetz <schuetz@dke.uni-linz.ac.at>
2022-11-02 14:21:32 +0100

    Enable OpenAPI definition (Swagger UI)

--- src/main/java/at/jku/dke/ida/idaquery/service/dto/AnalysisSituationDTO.java
+++ src/main/java/at/jku/dke/ida/idaquery/service/dto/AnalysisSituationDTO.java
@@ -1,4 +1,13 @@
 package at.jku.dke.ida.idaquery.service.dto;
 
 public class AnalysisSituationDTO {
+    private String test;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
 }
